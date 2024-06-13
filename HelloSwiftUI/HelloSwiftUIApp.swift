cf8a8eccd010cc6c0c32dd4a071fc66962f53ff5
Brandon <89342970+letsgobjones@users.noreply.github.com>
2024-06-13 13:56:27 +0900

    Initial Commit

---
+++ HelloSwiftUI/HelloSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloSwiftUIApp.swift
+//  HelloSwiftUI
+//
+//  Created by Brandon Jones on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
